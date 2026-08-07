d64bafe981832b7937245e9abca2897ba84dcbcf
Jürgen Hock <juergen.hock@jurihock.de>
2026-08-07 15:16:50 +0200

    Update version

--- voicesmith/build.gradle.kts
+++ voicesmith/build.gradle.kts
@@ -22,8 +22,8 @@ val sdk by extra(intArrayOf(33, 34, 36)) // minSdk <= targetSdk <= compileSdk
 val jvm by extra(1.8) // sourceCompatibility == targetCompatibility == jvmTarget
 
 // release version information
-val releaseVersionName by extra("3.0")
-val releaseVersionCode by extra(13)
+val releaseVersionName by extra("3.1")
+val releaseVersionCode by extra(14)
 
 plugins {
   alias(libs.plugins.android.gradle.plugin)
