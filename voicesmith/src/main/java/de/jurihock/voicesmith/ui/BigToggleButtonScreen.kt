99675b414b92a04cb8264a85b1bfe33ef8f88b8d
itMuench <it-muench@tuta.com>
2026-09-21 12:29:55 +0200

    Style disabled audio buttons

--- voicesmith/src/main/java/de/jurihock/voicesmith/ui/BigToggleButtonScreen.kt
+++ voicesmith/src/main/java/de/jurihock/voicesmith/ui/BigToggleButtonScreen.kt
@@ -3,6 +3,8 @@ package de.jurihock.voicesmith.ui
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
@@ -18,6 +20,9 @@ fun BigToggleButtonScreen(modifier: Modifier = Modifier,
   Button(
     modifier = modifier.fillMaxWidth(),
     enabled = enabled,
+    colors = ButtonDefaults.buttonColors(
+      disabledContainerColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f),
+      disabledContentColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f)),
     onClick = onToggle) {
     Text(
       text = if (value.value) textOff else textOn,
