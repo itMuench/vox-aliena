cd0ae61482e0e804d3b6edc4430b1e58b9b6e23a
itMuench <it-muench@tuta.com>
2026-09-21 10:45:16 +0200

    Display device names above selectors

--- voicesmith/src/main/java/de/jurihock/voicesmith/ui/DeviceSelectorScreen.kt
+++ voicesmith/src/main/java/de/jurihock/voicesmith/ui/DeviceSelectorScreen.kt
@@ -1,5 +1,6 @@
 package de.jurihock.voicesmith.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,30 +9,63 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 
 @Composable
 fun DeviceSelectorScreen(modifier: Modifier = Modifier,
                          textInput: String, textOutput: String,
                          textMono: String, textStereo: String,
+                         inputDevice: State<String>,
+                         outputDevice: State<String>,
                          channels: State<Int>,
                          onSelectInputDevice: () -> Unit,
                          onSelectOutputDevice: () -> Unit,
                          onSelectChannels: () -> Unit) {
 
-  Row(modifier = modifier.fillMaxWidth()) {
-    OutlinedButton(modifier = Modifier.weight(1f), onClick = onSelectInputDevice) {
-      Text(text = textInput)
-    }
+  Row(
+    modifier = modifier.fillMaxWidth(),
+    verticalAlignment = Alignment.Bottom
+  ) {
+    DeviceButton(
+      modifier = Modifier.weight(1f),
+      deviceName = inputDevice.value,
+      buttonText = textInput,
+      onClick = onSelectInputDevice)
     Spacer(modifier = Modifier.width(Dp(UI.PADDING)))
     OutlinedButton(modifier = Modifier.weight(0.5f), onClick = onSelectChannels) {
       Text(text = if (channels.value != 2) textMono else textStereo)
     }
     Spacer(modifier = Modifier.width(Dp(UI.PADDING)))
-    OutlinedButton(modifier = Modifier.weight(1f), onClick = onSelectOutputDevice) {
-      Text(text = textOutput)
-    }
+    DeviceButton(
+      modifier = Modifier.weight(1f),
+      deviceName = outputDevice.value,
+      buttonText = textOutput,
+      onClick = onSelectOutputDevice)
   }
 
+}
+
+@Composable
+private fun DeviceButton(modifier: Modifier = Modifier,
+                         deviceName: String,
+                         buttonText: String,
+                         onClick: () -> Unit) {
+  Column(
+    modifier = modifier,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Text(
+      modifier = Modifier.fillMaxWidth(),
+      text = deviceName,
+      maxLines = 1,
+      overflow = TextOverflow.Ellipsis,
+      textAlign = TextAlign.Center)
+    OutlinedButton(modifier = Modifier.fillMaxWidth(), onClick = onClick) {
+      Text(text = buttonText)
+    }
+  }
 }
