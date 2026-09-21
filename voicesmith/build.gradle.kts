23f4fc6e505b4a2f581844d95858abe7ed46a1ae
itMuench <it-muench@tuta.com>
2026-09-21 10:54:13 +0200

    Reset app version name to 1.0

--- voicesmith/build.gradle.kts
+++ voicesmith/build.gradle.kts
@@ -22,7 +22,7 @@ val sdk by extra(intArrayOf(33, 34, 36)) // minSdk <= targetSdk <= compileSdk
 val jvm by extra(1.8) // sourceCompatibility == targetCompatibility == jvmTarget
 
 // release version information
-val releaseVersionName by extra("3.1")
+val releaseVersionName by extra("1.0")
 val releaseVersionCode by extra(14)
 
 plugins {
